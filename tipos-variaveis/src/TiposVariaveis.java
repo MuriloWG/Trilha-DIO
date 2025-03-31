2f44ddb6cf0c685250da4c782832cbcacc2e73b7
MuriloWg <murilowg124@gmail.com>
2025-03-31 11:45:38 -0300

    Tipos variaveis primitivas em java

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,33 @@
+public class TiposVariaveis {
+   
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        // Tipos primitivos
+        // int, double, char, boolean, byte, short, long, float
+        // String (não é primitivo)
+        // Variáveis
+        // Tipos primitivos
+        int idade = 10;
+        double salario = 2000.0;
+        char letra = 'A';
+        boolean verdadeiro = true;
+        byte b = 127;
+        short s = 1000;
+        long l = 10000000000L;
+        float f = 10.0f;
+        
+        // String (não é primitivo)
+        String nome = "Lucas";
+        
+        // Imprimindo os valores das variáveis
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Letra: " + letra);
+        System.out.println("Verdadeiro: " + verdadeiro);
+        System.out.println("Byte: " + b);
+        System.out.println("Short: " + s);
+        System.out.println("Long: " + l);
+        System.out.println("Float: " + f);
+        System.out.println("Nome: " + nome);
+    }
+}
